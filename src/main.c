51bef7407b4e124be82aa119bdd1d07d21034b41
JimmyChin946 <Judes946@gmail.com>
2025-09-13 23:38:04 -0700

    functions

--- src/main.c
+++ src/main.c
@@ -6,18 +6,51 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+////////////////////////////
+/// Variable Declaration ///
+////////////////////////////
 
+// graphics
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 #define TILE_SIZE 20
 
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
+// movement
 int direction;
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
+
+// snake
+SDL_Point* snake;
+size_t snakeSize, snakeCap;
+
+
+
+/////////////////
+/// Functions ///
+/////////////////
+void addToSnake(SDL_Point point) {
+	if (snakeSize == snakeCap) {
+		size_t newCap = (snakeCap == 0) ? 1 : snakeCap * 2;
+		SDL_Point* tmp_snake = (SDL_Point*) realloc(snake, newCap * sizeof(SDL_Point));
+
+		if (tmp_snake == NULL) {
+			free(snake);
+			exit(1);
+		}
+
+		snake = tmp_snake;
+		snakeCap = newCap;
+	}
+
+	snake[snakeSize] = point;
+	snakeSize ++;
+}
 
 SDL_Point randomTile() {
 	return (SDL_Point) { 
@@ -26,11 +59,16 @@ SDL_Point randomTile() {
 	};
 }
 
+void resetGame() {
+	snakeSize = 0;
+	addToSnake(randomTile());
+	direction = (rand() % 4);
+}
+
 /////////////////
-/// CALLBACKS ///
+/// Callbacks ///
 /////////////////
 // no need for a main function loop anymore
-// SDL_Log("Couldn't initalize SDL: %s", SDL_GetError());
 
 // called at the beginning
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
@@ -56,9 +94,11 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 			return SDL_APP_SUCCESS;
 		case SDL_EVENT_KEY_DOWN:
 			switch(event->key.scancode) {
+				// Quitting the game
 				case SDL_SCANCODE_ESCAPE:
 				case SDL_SCANCODE_Q:
 					return SDL_APP_SUCCESS;
+				// moving the snake
 				case SDL_SCANCODE_UP:
 				case SDL_SCANCODE_K:
 					direction = UP;
@@ -90,7 +130,5 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 }
 
 // called at the end, and handles some of the exits
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-
-}
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
 
